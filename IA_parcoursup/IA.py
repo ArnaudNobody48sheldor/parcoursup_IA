73074e86e2f9b6511c4e8556dc32df5f50631fec
nobody48sheldor <lolmyizi@gmail.com>
2021-06-20 21:57:44 +0200

    little update

--- IA_parcoursup/IA.py
+++ IA_parcoursup/IA.py
@@ -3,11 +3,41 @@ import matplotlib.pyplot as plt
 from math import *
 import csv
 
+#oubliez pas de comenter le code, perso je déteste faire ca mais bon...
+
+#j ai l impression que l utf-8 est pas supporté, pour avoir les key ect des dict faut ouvrir dans excel sinon ca prend en compte l utf-8
+
 with open('database/fr-esr-parcoursup_mpsi.csv', 'r') as fichier:
     data = []
     for row in csv.DictReader(fichier, delimiter= ';'):
         data.append(row)
     fichier.close()
 
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+
+DATA = []
+
 for i in range(len(data)):
-    print(data[i]['Ã‰tablissement'])
+    DATA.append(data[i]['Session'])
+    DATA.append(data[i]['Ã‰tablissement'])
+    DATA.append(data[i]['Lien de la formation sur la plateforme Parcoursup'])
+    DATA.append(data[i]['CapacitÃ© de lâ€™Ã©tablissement par formation'])
+    DATA.append(data[i]['Effectif total des candidats pour une formation'])
+    DATA.append(data[i]['Effectif total des candidats ayant reÃ§u une proposition dâ€™admission de la part de lâ€™Ã©tablissement'])
+    DATA.append(data[i]['Effectif total des candidats ayant acceptÃ© la proposition de lâ€™Ã©tablissement (admis)'])
+    DATA.append(data[i]['Dont effectif des admis issus du mÃªme Ã©tablissement (BTS/CPGE)'])
+    DATA.append(data[i]['Dont effectif des admis issus de la mÃªme acadÃ©mie'])
+    DATA.append(data[i]['Rang du dernier appelÃ© du groupe 1'])
+    DATA.append(data[i]['Indicateur Parcoursup du taux dâ€™accÃ¨s des candidats ayant postulÃ© Ã  la formation (ratio entre le dernier appelÃ© et le dernier classÃ©)'])
+
+
+K = data[0].keys()
+Keys_list = []
+
+for i in K:
+    Keys_list.append(i)
+
+print(Keys_list)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# parcoursup_IA
-je suis un élève de (fin) de premiere et j essaye de faire une IA qui estime vos chances de rentrer dans l ecole/prepa/uni... que vous voulez à partir de la database du gouvernement en opendata
+## Qu'est-ce-que PARCOURSUP-IA ?
+Le but de ce code est d'essayer de déterminer les probabilités d'être prit dans une certaine école/prépa/uni... en renseignant certaine informations, le code vous dira les vos chances d'entrer dans x établissement dans le supérieur.
+![parcoursup logo](https://upload.wikimedia.org/wikipedia/fr/thumb/d/dc/Logo_parcoursup.svg/1200px-Logo_parcoursup.svg.png)
+## lancer le programme ?
+le programme étant écrite en python, il vous faudra donc python d'installé sur votre PC mais ce n'est pas la version définitive, donc il est possible que dans le futur ce soit sur un site web, une app PC ou même téléphone.
+
+![python](https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/200px-Python-logo-notext.svg.png)
+
+## Vous voulez participer ?
+Afin de participer, vous pouvez faire des commits au code existant, mais le projet étant à ses débuts les branches ne sont pas configurés.
+
+Sinon vous pouvez m'envoyer des messages
